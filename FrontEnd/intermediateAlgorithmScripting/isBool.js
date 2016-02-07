119d8a4ce103778d7d8dd83dcd558e5a7f993d87
Pablo Bordons Estrada <pboest@users.noreply.github.com>
2016-02-07 14:47:04 +0100

    Create isBool.js

---
+++ FrontEnd/intermediateAlgorithmScripting/isBool.js
@@ -0,0 +1,17 @@
+/*
+
+FCC:
+
+"Check if a value is classified as a boolean primitive. Return true or false."
+
+*/
+
+function isBool(bool) {
+  if(bool === true || bool === false){
+    return true;
+  }
+  else{
+    return false;
+  }
+
+}
