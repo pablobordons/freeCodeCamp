678bfee55dfd6b37c5be824ba722f7e7a38235cf
Pablo Bordons Estrada <pboest@users.noreply.github.com>
2016-03-07 10:04:03 +0100

    Create script.js

---
+++ FrontEnd/Projects/Advanced/TicTacToe/js/script.js
@@ -0,0 +1,23 @@
+
+//////////////////////////
+/////////////////////////
+//////
+/////
+////      Tic Tac Toe
+///
+//
+
+
+console.log("hey");
+
+var engine = {};
+
+var display = {};
+
+var ia = {};
+
+
+var moveO = '<div class="move move-circle"><i class="fa fa-circle-o"></i></div>';
+var moveX = '<div class="move move-cross"><i class="fa fa-times"></i></div>';
+
+$(".square").html(moveX);
